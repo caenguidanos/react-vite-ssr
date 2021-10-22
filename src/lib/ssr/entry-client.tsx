7b69f8d626c08b996d5a1677496a18b1d6425a87
Cristian Enguídanos Nebot <53697395+cenguidanos@users.noreply.github.com>
2021-10-22 18:30:32 +0200

    develop

--- src/lib/ssr/entry-client.tsx
+++ src/lib/ssr/entry-client.tsx
@@ -3,7 +3,7 @@ import { hydrate } from "react-dom";
 import Entry from "./main";
 import { SSRProvider } from "./ssr-context";
 
-const ssrDataEl = document.getElementById("__SSR_DATA__") as HTMLScriptElement;
+const ssrDataEl = document.getElementById("__APP_DATA__") as HTMLScriptElement;
 
 hydrate(
    <SSRProvider value={JSON.parse(ssrDataEl.innerText)}>
--- tailwind.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-   content: ["./index.html", "./src/**/*.tsx", "./src/**/*.ts"],
-   theme: {
-      extends: {},
-   },
-   variants: {},
-   plugins: [],
-};
